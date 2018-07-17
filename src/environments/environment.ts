8e0b44e5f24120f424e86920bd25df42d5f046f9
Jean Pierre Velez <vcjpierre@gmail.com>
2018-07-17 04:44:37 -0400

    Adding key for openweathermap.org

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  key: '9cafcc3cc7ba171585fc1375f67c3eeb'
 };
 
 /*
