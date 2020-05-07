ccc616a1d388a2041440bca5748e08cc94909cf9
Jean Pierre Velez <vcjpierre@gmail.com>
2020-05-07 03:19:06 -0400

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-require('dotenv').load();
-
 export const environment = {
-  production: true,
-  key: '${process.env.OPEN_WEATHER_API_KEY}'
-};
+         production: true,
+         key: '2ae076b67bc9f6ef77290e03934a16ca',
+       };
